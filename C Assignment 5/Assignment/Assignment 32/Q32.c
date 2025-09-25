f825c5d8dee7dac3bb9ea2ab3ba0669fe53a7cde
mananjani2102 <manan.jani.cg@gmail.com>
2025-09-25 12:48:14 +0530

    108649

--- C Assignment 5/Assignment/Assignment 32/Q32.c
+++ C Assignment 5/Assignment/Assignment 32/Q32.c
@@ -4,5 +4,16 @@ int main() {
     printf("Enter the number: ");
     scanf("%d", &n);
      
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            if(i==j || i==n || j==1){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
     return 0;
 }
--- C Assignment 5/Assignment/Assignment 36/Q36.c
+++ C Assignment 5/Assignment/Assignment 36/Q36.c
@@ -4,5 +4,14 @@ int main() {
     printf("Enter the number: ");
     scanf("%d", &n);
      
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf(" ");
+        }
+        for(int k=0;k<i;k++){
+            printf("%c",'A'+k);
+        }
+        printf("\n");
+    }
     return 0;
 }
